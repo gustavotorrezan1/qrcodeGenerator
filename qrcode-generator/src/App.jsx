1999f571686c8b647c35253b6f749d725b7a3b4f
gustavotorrezan1 <74027638+gustavotorrezan1@users.noreply.github.com>
2025-10-04 14:49:31 +0000

    refactor: remove Material-UI dependencies and components
    - Removed @mui/material and @mui/icons-material from package.json.
    - Updated App.jsx to eliminate Material-UI components (Button, TextField).
    - Deleted unused teste.jsx component that utilized Material-UI Button.
    - Added primereact as a new dependency for future UI components.

--- qrcode-generator/src/App.jsx
+++ qrcode-generator/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 
 export default function App() {
   return (
     <div className='app'>
-      <h1>QR Code Generator</h1>
-      <TextField sx={{ borderRadius: '0.375rem', border: '1px solid #ccc', width: '300px' }} id="filled-basic" label="Link" variant="filled" />
-      <Button variant="contained">Generate QR Code</Button>
+     
     </div>
   );
 }
